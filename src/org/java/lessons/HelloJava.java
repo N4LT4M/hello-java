7158bc05811b9ee98e5dee50b3b7b7d97e45a750
N4LT4M <m4tt3o.casale@gmail.com>
2023-07-11 12:43:11 +0200

    example

---
+++ src/org/java/lessons/HelloJava.java
@@ -0,0 +1,12 @@
+package org.java.lessons;
+/*
+commento su più righe
+molto carino
+ */
+public class HelloJava {
+//tutto il codice va scritto dentro alle parentesi graffe
+public static void main(String[] args) {
+    System.out.println("This is my first java program!");
+    System.out.println("same kind of print but made with sout shortcut");
+}
+}
